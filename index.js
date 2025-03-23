575e4031150a6705fe84556be3a047dbcd5f516a
RayhaaNako <71748270+RayhaaNako@users.noreply.github.com>
2025-03-24 06:42:21 +0700

    Finishing project and make responsive (not perfectly responsive yet)

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
+import moment from "moment";
+import Swal from "sweetalert2";
 
+//* Initialize Project
 const app = express();
 const port = 3000;
 const posts = [];
@@ -10,8 +13,7 @@ class Post {
     constructor(title, content) {
         this.title = title;
         this.content = content;
-        this.rawDate = new Date();
-        this.date = this.rawDate.toLocaleString();
+        this.date = moment().format('ddd, d/M/yyyy | HH:mm');
     }
 }
 
@@ -19,8 +21,6 @@ class Post {
 function addPost(title, content) {
     const post = new Post(title, content);
     posts.push(post);
-
-    console.log(posts);
 }
 
 //* Delete Post
@@ -37,18 +37,56 @@ function editPost(index, title, content) {
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//* Render the home screen
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {posts: posts});
 });
 
-app.post("/save", (req, res) => {
+//* Create post function
+app.post("/create", (req, res) => {
     let title = req.body["title"];
     let content = req.body["content"];
-
+    
     addPost(title, content);
     res.redirect("/");
 });
 
+//* Delete post function
+app.post("/delete/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    deletePost(id);
+    
+    res.redirect("/");
+});
+
+//* Render the selected post to view
+app.get("/view/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts[id];
+
+    res.render("view.ejs", {postId: id, title: post.title, content: post.content, date: post.date});
+});
+
+//* Render the edit form for updating selected post
+app.get("/edit/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts[id];
+
+    res.render("edit.ejs", {postId: id, title: post.title, content: post.content, date: post.date});
+});
+
+app.post("/update", (req, res) => {
+    let id = req.body["id"];
+    let title = req.body["title"];
+    let content = req.body["content"];
+    let date = req.body["date"]
+
+    editPost(id, title, content);
+    res.render("view.ejs", {postId: id, title: title, content: content, date: date});
+});
+
+//* Listener
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
